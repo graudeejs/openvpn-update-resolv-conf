b491b856775cbfcb3df639e54644156febdce426
Aldis Berjoza <aldis@berjoza.lv>
2018-02-21 00:54:22 +0200

    Rewrote update-resolv-conf.sh for FreeBSD

--- update-resolv-conf.sh
+++ update-resolv-conf.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-#
+#!/bin/sh
+
 # Parses DHCP options from openvpn to update resolv.conf
 # To use set as 'up' and 'down' script in your openvpn *.conf:
 # up /etc/openvpn/update-resolv-conf
@@ -8,6 +8,7 @@
 # Used snippets of resolvconf script by Thomas Hood <jdthood@yahoo.co.uk>
 # and Chris Hanson
 # Licensed under the GNU GPL.  See /usr/share/common-licenses/GPL.
+# 02/2018 aldis@berjoza.lv rewrote for sh on FreeBSD
 # 07/2013 colin@daedrum.net Fixed intet name
 # 05/2006 chlauber@bnc.ch
 #
@@ -21,50 +22,68 @@
 ## PATH below. You might need to directly set the path to 'resolvconf'
 ## manually if it still doesn't work, i.e.
 ## RESOLVCONF=/usr/sbin/resolvconf
-export PATH=$PATH:/sbin:/usr/sbin:/bin:/usr/bin
-RESOLVCONF=$(type -p resolvconf)
 
-case $script_type in
 
-up)
-  for optionname in ${!foreign_option_*} ; do
-    option="${!optionname}"
-    echo $option
-    part1=$(echo "$option" | cut -d " " -f 1)
-    if [ "$part1" == "dhcp-option" ] ; then
-      part2=$(echo "$option" | cut -d " " -f 2)
-      part3=$(echo "$option" | cut -d " " -f 3)
-      if [ "$part2" == "DNS" ] ; then
-        IF_DNS_NAMESERVERS="$IF_DNS_NAMESERVERS $part3"
-      fi
-      if [[ "$part2" == "DOMAIN" || "$part2" == "DOMAIN-SEARCH" ]] ; then
-        IF_DNS_SEARCH="$IF_DNS_SEARCH $part3"
-      fi
-    fi
-  done
-  R=""
-  if [ "$IF_DNS_SEARCH" ]; then
-    R="search "
-    for DS in $IF_DNS_SEARCH ; do
-      R="${R} $DS"
+RESOLVCONF='/sbin/resolvconf'
+ESED='/usr/bin/sed -E'
+
+if_dns_nameservers() {
+    env | grep '^foreign_option_' | while read -r line; do
+        option=$(echo $line | $ESED 's/[^=]+=//')
+        part1=$(echo "$option" | cut -d ' ' -f 1)
+        if [ "$part1" = 'dhcp-option' ]; then
+            part2=$(echo "$option" | cut -d ' ' -f 2)
+            if [ "$part2" = 'DNS' ]; then
+                part3=$(echo "$option" | cut -d ' ' -f 3)
+                printf ' %s' "$part3"
+            fi
+        fi
     done
-  R="${R}
-"
-  fi
+}
 
-  for NS in $IF_DNS_NAMESERVERS ; do
-    R="${R}nameserver $NS
-"
-  done
-  #echo -n "$R" | $RESOLVCONF -x -p -a "${dev}"
-  echo -n "$R" | $RESOLVCONF -x -a "${dev}.inet"
-  ;;
-down)
-  $RESOLVCONF -d "${dev}.inet"
-  ;;
+if_dns_search() {
+    env | grep '^foreign_option_' | while read -r line; do
+        option=$(echo $line | $ESED 's/[^=]+=//')
+        part1=$(echo "$option" | cut -d ' ' -f 1)
+        if [ "$part1" = 'dhcp-option' ]; then
+            part2=$(echo "$option" | cut -d ' ' -f 2)
+            if [ "$part2" = 'DOMAIN' -o "$part2" = 'DOMAIN-SEARCH' ]; then
+                part3=$(echo "$option" | cut -d ' ' -f 3)
+                printf ' %s' "$part3"
+            fi
+        fi
+    done
+}
+
+build_config() {
+    IF_DNS_NAMESERVERS=$(if_dns_nameservers)
+    IF_DNS_SEARCH=$(if_dns_search)
+
+    R=""
+    if [ "$IF_DNS_SEARCH" ]; then
+        for DS in $IF_DNS_SEARCH; do
+            R="${R} $DS"
+        done
+        R=$(printf '%s\n' "${R}")
+    fi
+
+    for NS in $IF_DNS_NAMESERVERS; do
+        R=$(printf '%s\n' "${R}nameserver $NS")
+    done
+    echo "$R"
+}
+
+case "$script_type" in
+"up")
+    build_config | $RESOLVCONF -x -a "${dev}.inet"
+    ;;
+
+"down")
+    $RESOLVCONF -d "${dev}.inet"
+    ;;
 esac
 
-# Workaround / jm@epiclabs.io 
+# Workaround / jm@epiclabs.io
 # force exit with no errors. Due to an apparent conflict with the Network Manager
 # $RESOLVCONF sometimes exits with error code 6 even though it has performed the
 # action correctly and OpenVPN shuts down.
